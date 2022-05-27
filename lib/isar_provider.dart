8d0c69d3030f032484021c73216de9e021f9738d
{sciotein} <{sciotein.job@gmail.com}>
2022-05-27 17:41:16 +0900

    small change

--- lib/isar_provider.dart
+++ lib/isar_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:isar/isar.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'post.dart';
 import 'post_service_model.dart';
 
